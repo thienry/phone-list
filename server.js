8011f685147acc90fa9aa397eca70d3e402cff1f
Thiago Moura <thmoura14@hotmail.com>
2019-08-06 23:22:38 -0300

    Configured conection to mongo database

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Conecta com o banco de dados
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Bem vindo à API da aplicação PHONE LIST" })
 );
 
+// Define as rotas da API
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/users", require("./routes/users"));
