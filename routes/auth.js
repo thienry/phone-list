fa0e6caadc7223defc7966346f0cdf1b1433cb12
Thiago Moura <thmoura14@hotmail.com>
2019-08-07 10:13:43 -0300

    Implemented authhenticate route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../models/User");
 
 /**
  *  @route    GET api/auth
@@ -15,8 +21,49 @@ router.get("/", (req, res) => {
  *  @desc     Autentica o usuário e pega o token
  *  @access   Público
  */
-router.post("/", (req, res) => {
-  res.send("Loga o usuário");
-});
+router.post(
+  "/",
+  [
+    check("email", "Por Favor insira um email válido!").isEmail(),
+    check("password", "Por Favor insira uma senha válida!").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) res.status(400).json({ msg: "Usuário ou senha errada!" });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) res.status(400).json({ msg: "Usuário ou senha errada!" })
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error!");
+    }
+  }
+);
 
 module.exports = router;
