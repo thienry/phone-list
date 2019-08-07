9e1dfb4fbc5d750aafa89f18257730fe4e9d2e81
Thiago Moura <thmoura14@hotmail.com>
2019-08-06 22:34:21 -0300

    Presentation router created for the API

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) =>
+  res.json({ msg: "Bem vindo à API da aplicação PHONE LIST" })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
