5b38c8df7c9a124ae3651baad9442f356c829e46
Thiago Moura <thmoura14@hotmail.com>
2019-08-08 16:54:23 -0300

    Auth context & initial state implemented

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,25 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
 import ContactState from "./context/contact/contactState";
+import AuthState from "./context/auth/AuthState";
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
