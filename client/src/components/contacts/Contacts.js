a51545572b62839ea57359038b6c26d76926f65b
Thiago Moura <thmoura14@hotmail.com>
2019-08-08 11:59:17 -0300

    Add & Delete Animation implemented

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import ContactContext from "../../context/contact/contactContext";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import ContactItem from "./ContactItem";
 
@@ -11,13 +12,19 @@ const Contacts = () => {
 
   return (
     <>
-      {filtered !== null
-        ? filtered.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        : contacts.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </>
   );
 };
