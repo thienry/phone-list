cdd527f0cfc25c0902ae91c2b202dd6fb1f218e7
Thiago Moura <thmoura14@hotmail.com>
2019-08-09 03:20:43 -0300

    Prepare for deploy the app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -13,14 +14,18 @@ app.use(cors());
 // Inicializa o Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Bem vindo à API da aplicação PHONE LIST" })
-);
-
 // Define as rotas da API
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/users", require("./routes/users"));
+
+// Serve ativos estáticos na produção
+if (process.env.NODE_ENV === "production") {
+  // Defini pasta estática
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 const PORT = process.env.PORT || 5000;
 
